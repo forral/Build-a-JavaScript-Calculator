2de3cb2735b99cd2acd881302ec560cdfcb79428
forral <forra.luis@me.com>
2017-11-24 15:46:12 -0600

    Adding the basic visual functionality of the input: the default value of 0, when ESC clear the input and put the default value, when backspace until there is no value in the input, then insert the default value. Always onFocus the input even when the user clicks somewehe else on the page. Removing the blinking cursor on the input and remove the default color of the highlight.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,37 @@
-console.log('connected');
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function clear(element) {
+  element.value = 0;
+  input.select();
+}
+
+var input = document.querySelector('input');
+input.value = 0;
+input.focus();
+input.select();
+
+input.addEventListener('keydown', function() {
+  if (this.value === 0) {
+    this.valus = '';
+  }
+});
+
+input.addEventListener('keyup', function(e) {
+  if (this.value === '' || e.keyCode === 27) {
+    clear(this);
+  }
+});
