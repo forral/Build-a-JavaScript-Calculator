e0ccbb5f6d316c107713a5bb0a6bbff36db00077
forral <forra.luis@me.com>
2017-11-28 13:00:21 -0600

    The app makes simple math and inputs to the page

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
 var input = document.querySelector('input');
 var keyboard = document.querySelectorAll('.keyboard button');
 
+var operation = [];
+
 function clickHandles(e) {
 
   if (this.dataset.value === 'AC') {
     input.value = '';
+    operation = [];
+
+  } else if (this.dataset.value === '=') {
+    operation.push(input.value);
+    input.value = (eval(operation.join(' ')));
+    console.log(operation);
+    
 
   } else {
     input.value = input.value + this.dataset.value;
