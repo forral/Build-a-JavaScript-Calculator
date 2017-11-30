abfb64df5ad2d610033b6d288580c6669faff866
forral <forra.luis@me.com>
2017-11-30 12:18:29 -0600

    Replace the input to an div

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
-var input = document.querySelector('input');
+var screen = document.querySelector('.screen');
 var keyboard = document.querySelectorAll('.keyboard button');
 
 // Init.
-// Giving the input the default value of 0 and highlight it.
-input.value = '0';
-input.select();
-
+screen.textContent = '0';
 var operation = [];
 
 function clickHandles(e) {
@@ -13,30 +10,28 @@ function clickHandles(e) {
   // If the button pushed is AC, then put the 0 value into the input, highlight it and clean the operation array
   // **TODO**: turn into a function.
   if (this.dataset.value === 'AC') {
-    input.value = '0';
-    input.select();
+    screen.textContent = '0';
     operation = [];
-    console.dir(input);
     
   // If the button pushed is =, then push the current value in the input into the operation array, 
   //calculate it with eval() and show the result on the input.
   // **TODO**: turn into a function.
   } else if (this.dataset.value === '=') {
-    operation.push(input.value);
-    input.value = (eval(operation.join(' ')));
+    operation.push(screen.textContent);
+    screen.textContent = (eval(operation.join(' ')));
     operation = [];
   
   // If the input selected value it's 0 and the button pressed it's '.' the append it to the current value.
-  } else if (window.getSelection().toString() === '0' && this.dataset.value === '.') {
-    input.value = input.value + this.dataset.value;
+  } else if (screen.textContent === '0' && this.dataset.value === '.') {
+    screen.textContent += this.dataset.value;
 
   // If the current select text in the input it's a 0, then clean it and insert the current pushed button value.
-  } else if (window.getSelection().toString() === '0') {
-    input.value = this.dataset.value;
+  } else if (screen.textContent === '0') {
+    screen.textContent = this.dataset.value;
 
   // show the button pressed value into the input.
   } else {
-    input.value = input.value + this.dataset.value;
+    screen.textContent += this.dataset.value;
   }
 }
 
