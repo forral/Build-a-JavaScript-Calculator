8799b09c9a6a83776d8fbb1e404a71d5a15d4c82
forral <forra.luis@me.com>
2017-11-30 11:02:05 -0600

    Removing the default value of 0 from the HTML input tag and replace the functionality with Javascript.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
 var input = document.querySelector('input');
 var keyboard = document.querySelectorAll('.keyboard button');
 
+input.value = '0';
+input.select();
+
 var operation = [];
 
 function clickHandles(e) {
 
   if (this.dataset.value === 'AC') {
-    input.value = '';
+    input.value = '0';
+    input.select();
     operation = [];
-
+    console.dir(input);
+    
   } else if (this.dataset.value === '=') {
     operation.push(input.value);
     input.value = (eval(operation.join(' ')));
     operation = [];
+  
+  } else if (window.getSelection().toString() === '0') {
+    input.value = this.dataset.value;
     
   } else {
     input.value = input.value + this.dataset.value;
