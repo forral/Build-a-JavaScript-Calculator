f0abb72957c0cb267818e2cc58ed2d1d81a4927f
forral <forra.luis@me.com>
2017-12-03 21:00:43 -0600

    Adding data-type for the differrent numbers in the html button tag

--- scripts.js
+++ scripts.js
@@ -5,49 +5,59 @@ var keyboard = document.querySelectorAll('.keyboard button');
 screen.textContent = '0';
 var operation = [];
 
-var currentValue = '';
-var currentOperation = '';
+var currentValue;
+var currentOperation;
 
 function clickHandles(e) {
 
-  // If the button pushed is AC, then put the 0 value into the .screen and clean the operation array.
-  // **TODO**: turn into a function.
-  if (this.dataset.value === 'AC') {
-    screen.textContent = '0';
-    operation = [];
-    
-  // If the button pushed is =, then push the current value in the .screen into the operation array, 
-  //calculate it with eval() and show the result on the .screen.
-  // **TODO**: turn into a function.
-  } else if (this.dataset.value === '=') {
-    operation.push(screen.textContent);
-    screen.textContent = (eval(operation.join(' ')));
-    operation = [];
-  
-  // If the screen value it's 0 and the button pressed it's '.' the append it to the current value.
-  } else if (screen.textContent === '0' && this.dataset.value === '.') {
-    screen.textContent += this.dataset.value;
-
-  // If the current value in the .screen it's a 0, then clean it and insert the current pushed button value.
-  } else if (screen.textContent === '0') {
-    screen.textContent = this.dataset.value;
-
-  
-  } else if (this.dataset.value === '+') {
-    currentValue = screen.textContent;
-    currentOperation = this.dataset.value;
-    console.log(currentOperation);
-    console.log(currentValue);
-    
-    
-    
-
+  if (this.dataset.type === 'mathOperation') {
+    console.log('math operation');
   }
+
+
+  // // If the button pushed is AC, then put the 0 value into the .screen and clean the operation array.
+  // // **TODO**: turn into a function.
+  // if (this.dataset.value === 'AC') {
+  //   screen.textContent = '0';
+  //   operation = [];
+  //   currentValue = '';
+  //   currentOperation = '';
+    
+  // // If the button pushed is =, then push the current value in the .screen into the operation array, 
+  // //calculate it with eval() and show the result on the .screen.
+  // // **TODO**: turn into a function.
+  // } else if (this.dataset.value === '=') {
+  //   operation.push(screen.textContent);
+  //   screen.textContent = (eval(operation.join(' ')));
+  //   operation = [];
   
-  // show the button pressed value into the .screen.
-  else {
-    screen.textContent += this.dataset.value;
-  }
+  // // If the screen value it's 0 and the button pressed it's '.' the append it to the current value.
+  // } else if (screen.textContent === '0' && this.dataset.value === '.') {
+  //   screen.textContent += this.dataset.value;
+
+  // // If the current value in the .screen it's a 0, then clean it and insert the current pushed button value.
+  // } else if (screen.textContent === '0') {
+  //   screen.textContent = this.dataset.value;
+  
+  // } else if (this.dataset.value === '+') {
+    
+  //   if (currentOperation === undefined && currentValue === undefined) {
+  //     currentValue = screen.textContent;
+  //     currentOperation = this.dataset.value;
+
+  //     operation.push(currentValue);
+  //     operation.push(currentOperation);
+     
+  //   }
+
+
+  // }
+  
+  // // show the button pressed value into the .screen.
+  // else {
+  //   screen.textContent += this.dataset.value;
+  // }
+
 }
 
 keyboard.forEach(button => {
