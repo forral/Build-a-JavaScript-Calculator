660b9fb4d0b84b38c5bf479d3a5ad1882fd5ef63
forral <forra.luis@me.com>
2017-12-05 13:33:24 -0600

    Cleaning the js code

--- scripts.js
+++ scripts.js
@@ -26,6 +26,15 @@ function clickHandles(e) {
       // If the current value in the ´.screen´ it's a 0, then clean it and insert the current pushed button value.
       screen.textContent = this.dataset.value;
       }
+    } else if (this.dataset.value === '.') {
+      if (screen.textContent.indexOf('.') > 0) {
+        // If there is allready a point in the screen, don't let show more.
+        screen.textContent = screen.textContent;
+      }
+      if (screen.textContent === '') {
+        // If the screen don't have any value and the button click is a point, then put an 0 before it.
+        screen.textContent = '0' + this.dataset.value;
+      }
     } else {
       // show the clicked button value into the .screen.
       screen.textContent += this.dataset.value;
@@ -68,49 +77,6 @@ function clickHandles(e) {
     currentValue = '';
     currentOperation = '';
   }
-
-  // // If the button pushed is AC, then put the 0 value into the .screen and clean the operation array.
-  // // **TODO**: turn into a function.
-  // if (this.dataset.value === 'AC') {
-  //   screen.textContent = '0';
-  //   operation = [];
-  //   currentValue = '';
-  //   currentOperation = '';
-    
-  // // If the button pushed is =, then push the current value in the .screen into the operation array, 
-  // //calculate it with eval() and show the result on the .screen.
-  // // **TODO**: turn into a function.
-  // } else if (this.dataset.value === '=') {
-  //   operation.push(screen.textContent);
-  //   screen.textContent = (eval(operation.join(' ')));
-  //   operation = [];
-  
-  // // If the screen value it's 0 and the button pressed it's '.' the append it to the current value.
-  // } else if (screen.textContent === '0' && this.dataset.value === '.') {
-  //   screen.textContent += this.dataset.value;
-
-  // // If the current value in the .screen it's a 0, then clean it and insert the current pushed button value.
-  // } else if (screen.textContent === '0') {
-  //   screen.textContent = this.dataset.value;
-  
-  // } else if (this.dataset.value === '+') {
-    
-  //   if (currentOperation === undefined && currentValue === undefined) {
-  //     currentValue = screen.textContent;
-  //     currentOperation = this.dataset.value;
-
-  //     operation.push(currentValue);
-  //     operation.push(currentOperation);
-     
-  //   }
-
-
-  // }
-  
-  // // show the button pressed value into the .screen.
-  // else {
-  //   screen.textContent += this.dataset.value;
-  // }
 
 }
 
